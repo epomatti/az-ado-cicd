cfb21563d9544276df1c0665cf542f0e48e0ae65
Evandro Pomatti <evandro.pomatti@gmail.com>
2023-06-23 16:23:46 -0300

    static web app

--- azure/main.tf
+++ azure/main.tf
@@ -108,62 +108,12 @@ resource "azurerm_linux_web_app" "main" {
   }
 }
 
-# ### Frontend App Registration ###
+### Frontend  ###
 
-# resource "random_uuid" "oauth2_permission_scope_frontend" {}
-
-# resource "azuread_application" "frontend" {
-#   display_name     = "spafrontend"
-#   identifier_uris  = ["api://spafrontend"]
-#   sign_in_audience = "AzureADMyOrg"
-#   owners           = [local.owner_object_id]
-
-#   single_page_application {
-#     redirect_uris = ["http://localhost:4200/", var.staticsite_redirect_uri]
-#   }
-
-#   api {
-#     requested_access_token_version = 2
-
-#     oauth2_permission_scope {
-#       id                         = random_uuid.oauth2_permission_scope_frontend.result
-#       enabled                    = true
-#       type                       = "User"
-#       admin_consent_display_name = "Admin Consent"
-#       admin_consent_description  = "Admin Consent description"
-#       value                      = "user_impersonation"
-#     }
-#   }
-
-#   required_resource_access {
-#     resource_app_id = local.msgraph_application_id
-
-#     resource_access {
-#       id   = azuread_service_principal.msgraph.oauth2_permission_scope_ids["User.Read"]
-#       type = "Scope"
-#     }
-#   }
-
-#   required_resource_access {
-#     resource_app_id = azuread_application.backend.application_id
-
-#     resource_access {
-#       id   = azuread_application.backend.oauth2_permission_scope_ids["user_impersonation"]
-#       type = "Scope"
-#     }
-#   }
-# }
-
-# resource "azuread_service_principal" "frontend" {
-#   application_id               = azuread_application.frontend.application_id
-#   app_role_assignment_required = false
-#   owners                       = [local.owner_object_id]
-
-#   feature_tags {
-#     enterprise = true
-#   }
-# }
-
-# resource "azuread_application_password" "frontend" {
-#   application_object_id = azuread_application.frontend.object_id
-# }
+resource "azurerm_static_site" "frontend" {
+  name                = "stapp-${var.workload}"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = "West Europe"
+  sku_size            = "Free"
+  sku_tier            = "Free"
+}
